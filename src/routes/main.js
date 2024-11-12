814e08a6afe83f4e1154333cb8cb171544148d9e
xurshid soliyev <hunter982019@gmail.com>
2024-11-12 22:20:02 +0500

    update: some changes

--- src/routes/main.js
+++ src/routes/main.js
@@ -2,9 +2,9 @@ const express=require('express');
 const router=express.Router();
 const path=require('path');
 
-router.get('/blog/articles',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'articles', 'javascript.html'));
-})
 
+router.get('/',(req,res)=>{
+  res.render('main');
+})
 
 module.exports=router;
